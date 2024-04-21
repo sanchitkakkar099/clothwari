a24226b297e718eb764f82d4ed179a121f070908
Hardik-98 <86848666+Hardik-98@users.noreply.github.com>
2024-04-21 14:03:35 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === 'production';
 export default defineConfig({
   plugins: [react()],
   define:{
-    global:'window'
+    global: !isProduction ? {} : undefined,
   }
 })
